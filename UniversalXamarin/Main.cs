a3c8a46e5e206c7736415e881ac50aab4ac1a75a
Dennis Yarborough <swiftdev@icloud.com>
2016-07-20 19:07:13 -0400

    initial commit

---
+++ UniversalXamarin/Main.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+
+namespace UniversalXamarin
+{
+    public class Application
+    {
+        static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+
+    }
+}
